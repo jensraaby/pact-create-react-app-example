50b7659986e898c18a70d6a890b665b039b987d5
Jens Raaby <jensraaby@users.noreply.github.com>
2017-09-30 18:04:16 +0100

    Upgrade Pact and C-R-A to see if bug still exists

--- src/__tests__/pactTest.js
+++ src/__tests__/pactTest.js
@@ -2,15 +2,13 @@
 
 const path = require('path')
 const Pact = require('pact')
-const wrapper = require('@pact-foundation/pact-node')
 const getMeDogs = require('../pactClient').getMeDogs
 
 describe("Dog's API", () => {
   let url = 'http://localhost'
-  let provider
 
   const port = 8989;
-  const server = wrapper.createServer({
+  const provider = Pact({
     port: port,
     log: path.resolve(process.cwd(), 'logs', 'mockserver-integration.log'),
     dir: path.resolve(process.cwd(), 'pacts'),
@@ -21,23 +19,12 @@ describe("Dog's API", () => {
 
   const EXPECTED_BODY = [{dog: 1}]
 
-  afterAll(() => {
-    wrapper.removeAllServers();
-  })
+  beforeAll(() => provider.setup())
+  afterAll(() => provider.finalize())
 
-  afterEach(done => {
-    server.delete().then(done)
-  });
-
-  beforeEach(done => {
-    server.start().then(() => {
-      provider = Pact({ consumer: 'MyConsumer', provider: 'MyProvider', port: port })
-      done()
-    })
-  });
 
   describe("works", () => {
-    beforeEach(done => {
+    beforeAll(done => {
       const interaction = {
         state: 'i have a list of projects',
         uponReceiving: 'a request for projects',
@@ -55,19 +42,16 @@ describe("Dog's API", () => {
       provider.addInteraction(interaction).then(done, done)
     })
 
-    afterEach(done => {
-       return provider.finalize().then(done, done)
-    })
-
-    it('successfully verifies', done => {
+    it('returns a successful body', done => {
       return getMeDogs({ url, port })
-        .then(provider.verify)
         .then(response => {
           expect(response.headers['content-type']).toEqual('application/json');
           expect(response.data).toEqual(EXPECTED_BODY);
           expect(response.status).toEqual(200);
+          done();
         })
-        .then(done, done)
     })
+
+    it('successfully verifies', () => provider.verify())
   })
 })
